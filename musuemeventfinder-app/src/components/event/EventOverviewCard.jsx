acce5b48fbe7fff93f026b18a290c5ed8e1e3dfe
pascalheikamp <pascalheikamp17@gmail.com>
2024-08-03 14:49:34 +0200

    adding extra event to check layout

--- musuemeventfinder-app/src/components/event/EventOverviewCard.jsx
+++ musuemeventfinder-app/src/components/event/EventOverviewCard.jsx
@@ -1,6 +1,8 @@
 import {Image, StyleSheet, Text, View} from "react-native";
 import Calendar from '../../../assets/calendar.png';
 import Time from '../../../assets/time.png';
+import Bookmark from '../../../assets/bookmark.png';
+import Heart from '../../../assets/heart.png';
 
 function EventOverviewCard({title, img, description, date, begin, end}) {
 
@@ -13,16 +15,21 @@ function EventOverviewCard({title, img, description, date, begin, end}) {
             <View style={styles.InfoContainer}>
                 <View style={styles.Wrapper}>
                     <Image  source={Calendar}/>
-                    <Text>{begin}</Text>
+                    <Text style={styles.InfoItem}>{date}</Text>
                 </View>
                 <View style={styles.Wrapper}>
                     <Image source={Time} />
-                    <Text>Hello</Text>
+                    <Text style={styles.InfoItem}>{begin} - {end}</Text>
                 </View>
             </View>
             <View style={styles.InfoContainer}>
             </View>
             <Text style={styles.EventDescription}>{description}</Text>
+            <View style={styles.BottomSection}>
+                <Image source={Bookmark}/>
+                <Image source={Heart}/>
+                <Text>0</Text>
+            </View>
         </View>
     )
 }
@@ -30,22 +37,28 @@ function EventOverviewCard({title, img, description, date, begin, end}) {
 const styles =  StyleSheet.create({
     EventCard: {
         paddingBottom:"5%",
-        marginTop:"10%",
         paddingLeft:"5%",
         paddingRight:"5%",
         justifyContent:"center",
-        display:"flex",
         backgroundColor:"white",
-        height:"auto%"
+    },
+    BottomSection:{
+      display:"flex",
+        flexDirection:"row",
+      justifyContent:"flex-end"
     },
     Wrapper: {
         display:"flex",
-        flexDirection:"column"
+        flexDirection:"row",
     },
     InfoContainer: {
+        marginTop:"5%",
         justifyContent:"space-around",
         display:"flex",
         flexDirection:"row"
+    },
+    InfoItem: {
+        paddingLeft:"5%"
     },
     shadowProp: {
         shadowColor: '#171717',
@@ -56,10 +69,11 @@ const styles =  StyleSheet.create({
     EventImage:{
       width:"auto",
         marginTop:"5%",
-      height:"65%"
+      height:"50%"
     },
     EventTitle: {
         fontSize:"20%",
+        marginTop:"7%",
         textAlign:"center"
     },
     EventDescription: {
--- musuemeventfinder-app/src/screens/EventScreen.jsx
+++ musuemeventfinder-app/src/screens/EventScreen.jsx
@@ -1,43 +1,66 @@
-import {StyleSheet, Text, View} from "react-native";
-import {FlatGrid, SimpleGrid} from 'react-native-super-grid';
+import {ScrollView, StyleSheet, Text, View} from "react-native";
+import { FlatGrid } from 'react-native-super-grid';
 import EventOverviewCard from "../components/event/EventOverviewCard";
 import { LinearGradient } from 'expo-linear-gradient';
 
-
-function EventScreen ({route}) {
-    const {id, events, name} = route.params || {};
+function EventScreen({ route }) {
+    const { id, events, name } = route.params || {};
 
     const currentEvents = events.map((e) => (e));
-    console.log(currentEvents)
+    console.log(currentEvents);
 
     return (
-        <View style={styles.Container}>
-            <Text style={styles.Title}>The events of {name}</Text>
+        <View style={styles.container}>
+            <Text style={styles.title}>The events of {name}</Text>
             <LinearGradient
-                // Background Linear Gradient
                 colors={['rgba(26,29,46,1)', 'rgba(75,116,168,1)']}
                 style={styles.background}
             >
-            <FlatGrid style={styles.List}
-                itemDimension={200}
-                data={currentEvents}
-                      renderItem={({ item }) => (<EventOverviewCard title={item.event} description={item.description} date={item.date} begin={item.begin_time} end={item.end_time} img={item.image_url}> </EventOverviewCard>)}
-            />
+                <ScrollView style={styles.scrollView}>
+                <FlatGrid
+                    itemDimension={150}
+                    data={currentEvents}
+                    renderItem={({ item }) => (
+                        <EventOverviewCard
+                            title={item.event}
+                            description={item.description}
+                            date={item.date}
+                            begin={item.begin_time}
+                            end={item.end_time}
+                            img={item.image_url}
+                        />
+                    )}
+                    style={styles.list}
+                />
+                </ScrollView>
             </LinearGradient>
         </View>
-    )
+    );
 }
-const styles =  StyleSheet.create({
-    Container: {
-        flex:1,
-        marginBottom:"5%",
-        backgroundColor:"#2e517e",
-        alignItems:"center"
-    },
 
-    Title:{
-      fontSize:"24%",
-        color:"white"
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#2e517e",
+        alignItems: "center"
+    },
+    title: {
+        fontSize: 24,
+        color: "white",
+        marginVertical: 10
+    },
+    scrollView: {
+        flex: 1,
+        width: '100%',
+    },
+    background: {
+        flex: 1,
+        width: '100%',
+    },
+    list: {
+        flex: 1,
+        width: '100%',
     }
-})
+});
+
 export default EventScreen;
